f111a7fdcc769d609460caf7a5eb8324989f2097
enrique7mc <enrique7mc@gmail.com>
2014-12-30 16:34:20 -0600

    Añade Motivación Tipos Genéricos

---
+++ C# Intermedio/Inicial/MotivacionTiposGenericos/MotivacionTiposGenericos/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MotivacionTiposGenericos
+{
+    /// <summary>
+    /// Ejemplo de motivación para utilizar tipos genéricos
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ C# Intermedio/MotivacionTiposGenericos/MotivacionTiposGenericos/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MotivacionTiposGenericos
+{
+    /// <summary>
+    /// Ejemplo de motivación para utilizar tipos genéricos
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // crear un arreglo de int, double y char
+            int[] intArray = { 1, 2, 3, 4, 5, 6 };
+            double[] doubleArray = { 1.1, 2.2, 3.3, 4.4, 5.5, 6.6, 7.7 };
+            char[] charArray = { 'H', 'E', 'L', 'L', 'O' };
+
+            Console.WriteLine("intArray contiene:");
+            MuestraArreglo(intArray);
+
+            Console.WriteLine("doubleArray contiene:");
+            MuestraArreglo(doubleArray);
+
+            Console.WriteLine("charArray contiene:");
+            MuestraArreglo(charArray); 
+        }
+
+        // muestra arreglo int
+        private static void MuestraArreglo(int[] arreglo)
+        {
+            foreach (int element in arreglo)
+                Console.Write(element + " ");
+
+            Console.WriteLine("\n");
+        }
+
+        // muestra arreglo double
+        private static void MuestraArreglo(double[] arreglo)
+        {
+            foreach (double element in arreglo)
+                Console.Write(element + " ");
+
+            Console.WriteLine("\n");
+        }
+
+        // muestra arreglo char
+        private static void MuestraArreglo(char[] arreglo)
+        {
+            foreach (char element in arreglo)
+                Console.Write(element + " ");
+
+            Console.WriteLine("\n");
+        }
+    }
+}

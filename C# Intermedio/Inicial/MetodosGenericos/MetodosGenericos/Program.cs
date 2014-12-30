4049b705767e3f0ba62ae6e0f0b9451ba7f612f6
enrique7mc <enrique7mc@gmail.com>
2014-12-30 16:42:40 -0600

    Añade Métodos Genéricos
    C# Intermedio

---
+++ C# Intermedio/Inicial/MetodosGenericos/MetodosGenericos/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MetodosGenericos
+{
+    /// <summary>
+    /// Métodos Genéricos
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // crear un arreglo de int, double y char
+            int[] intArray = { 1, 2, 3, 4, 5, 6 };
+            double[] doubleArray = { 1.1, 2.2, 3.3, 4.4, 5.5, 6.6, 7.7 };
+            char[] charArray = { 'H', 'E', 'L', 'L', 'O' };
+
+            Console.WriteLine("intArray contiene:");
+            // MuestraArreglo(intArray);
+
+            Console.WriteLine("doubleArray contiene:");
+            // MuestraArreglo(doubleArray);
+
+            Console.WriteLine("charArray contiene:");
+            // MuestraArreglo(charArray);
+        }
+    }
+}
---
+++ C# Intermedio/MetodosGenericos/MetodosGenericos/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MetodosGenericos
+{
+    /// <summary>
+    /// Métodos Genéricos
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // crear un arreglo de int, double y char
+            int[] intArray = { 1, 2, 3, 4, 5, 6 };
+            double[] doubleArray = { 1.1, 2.2, 3.3, 4.4, 5.5, 6.6, 7.7 };
+            char[] charArray = { 'H', 'E', 'L', 'L', 'O' };
+
+            Console.WriteLine("intArray contiene:");
+            MuestraArreglo(intArray);
+
+            Console.WriteLine("doubleArray contiene:");
+            MuestraArreglo(doubleArray);
+
+            Console.WriteLine("charArray contiene:");
+            MuestraArreglo(charArray);
+        }
+
+        // método genérico para mostrar un arreglo
+        private static void MuestraArreglo<T>(T[] inputArray)
+        {
+            foreach (T element in inputArray)
+                Console.Write(element + " ");
+
+            Console.WriteLine("\n");
+        }
+    }
+}
